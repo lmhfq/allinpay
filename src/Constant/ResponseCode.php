bec49eaba9a9e18ad81ba90ae4fa1ec1f711caa1
lmh <lmh@weiyian.com>
2022-03-08 13:48:24 +0800

    通联支付sdk

---
+++ src/Constant/ResponseCode.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+/**
+ * Created by PhpStorm.
+ * User: lmh <lmh@weiyian.com>
+ * Date: 2022/3/8
+ * Time: 下午1:47
+ */
+
+namespace Lmh\AllinPay\Constant;
+
+
+class ResponseCode
+{
+    public const SUCCESS = 'SUCCESS';
+}
