bacc08de35cf92fb59bd475fa19c26b28214186b
lmh <lmh@weiyian.com>
2022-03-12 16:29:20 +0800

    通联海关sdk

--- src/Service/Customs/Response/OrderApplyResponse.php
+++ src/Service/Customs/Response/OrderApplyResponse.php
@@ -10,7 +10,32 @@ declare(strict_types=1);
 namespace Lmh\AllinPay\Service\Customs\Response;
 
 
+use Illuminate\Support\Arr;
+use Lmh\AllinPay\Constant\ReturnCode;
+
 class OrderApplyResponse extends BaseResponse
 {
 
+    protected $verDept;
+
+    /**
+     * @return mixed
+     */
+    public function getVerDept()
+    {
+        return $this->verDept;
+    }
+
+
+    /**
+     * @param string $message
+     * @author lmh
+     */
+    public function handle(string $message)
+    {
+        parent::handle($message);
+        if ($this->returnCode == ReturnCode::SUCCESS) {
+            $this->verDept = intval(Arr::get($this->responseBody, 'VER_DEPT', 0));
+        }
+    }
 }
